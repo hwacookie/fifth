cf015ea390a7924cb33f3154e610db6153b1c50c
hauke <hauke.walden@gmail.com>
2018-07-26 16:08:01 +0200

    another commit

--- src/main/java/de/biotronik/fifth/HelloWorldController.java
+++ src/main/java/de/biotronik/fifth/HelloWorldController.java
@@ -15,6 +15,11 @@ public class HelloWorldController {
 
     @GetMapping("/hello-world")
     @ResponseBody
+    /**
+     * Aha!
+     * @param name
+     * @return
+     */
     public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
